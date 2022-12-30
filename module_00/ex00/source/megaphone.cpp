47abec2a116ab6186310a2b7ba998719c9f5663f
Thomas Da Silva <thomas-78000@hotmail.fr>
2022-12-30 22:29:50 +0100

    module_00 ex00

--- module_00/ex00/source/megaphone.cpp
+++ module_00/ex00/source/megaphone.cpp
@@ -6,14 +6,46 @@
 /*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 09:00:46 by tda-silv          #+#    #+#             */
-/*   Updated: 2022/12/30 09:33:32 by tda-silv         ###   ########.fr       */
+/*   Updated: 2022/12/30 22:28:21 by tda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <stdio.h>
 
-int	main()
+
+int	*str_toupper_cpp(char *str);
+
+int	main(int argc, char **argv)
 {
-	std::cout << "Hello world !" << std::endl;
+	int	i;
+
+	i = 1;
+	if (argc > 1)
+	{
+		while (argv[i])
+			str_toupper_cpp(argv[i++]);
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (0);
+}
+
+int	*str_toupper_cpp(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			std::cout << (char)(str[i] - 32);
+		else
+			std::cout << (char)str[i];
+		i++;
+	}
 	return (0);
 }
