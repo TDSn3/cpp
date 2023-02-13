016aedad91673027f0344ca3d40287eca6b59a9b
Thomas Da Silva <thomas-78000@hotmail.fr>
2023-02-13 11:46:34 +0100

    m07 ex02 started

---
+++ module_07/ex02/include/Array.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   template.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/12 14:49:55 by tda-silv          #+#    #+#             */
+/*   Updated: 2023/02/13 11:35:10 by tda-silv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+# include <string>
+# include <exception>
+# include <cctype>
+
+template<typename T>
+class Array
+{
+	public:
+
+		Array(void);
+		Array(T);
+		Array(const Array &src);
+		~Array(void);
+	
+		Array	&operator = (const Array &right);
+		
+	protected:
+
+	private:
+
+};
+
+#endif
---
+++ module_07/ex02/include/header.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/11 15:58:36 by tda-silv          #+#    #+#             */
+/*   Updated: 2023/02/13 11:38:30 by tda-silv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_HPP
+# define HEADER_HPP
+
+# include <iostream>
+# include <string>
+# include <exception>
+# include <cctype>
+
+# include "Array.hpp"
+
+#endif
