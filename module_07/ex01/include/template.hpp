e04a4442e8a612033ef5b73c7019ba5653316957
Thomas Da Silva <thomas-78000@hotmail.fr>
2023-02-13 11:25:59 +0100

    m07 ex01 ended

--- module_07/ex01/include/template.hpp
+++ module_07/ex01/include/template.hpp
@@ -6,7 +6,7 @@
 /*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 14:49:55 by tda-silv          #+#    #+#             */
-/*   Updated: 2023/02/13 10:17:09 by tda-silv         ###   ########.fr       */
+/*   Updated: 2023/02/13 11:20:24 by tda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@
 # include <exception>
 # include <cctype>
 
-template<typename T, size_t, typename U>
-void	iter(T *tab, const size_t &tab_size, U (*function_a)())
+template<typename T>
+void	iter(T *tab, size_t tab_size, void (*function_a)(T))
 {
 	size_t	i = 0;
 
 	while (i < tab_size)
-		(*function_a)(tab[i++]);
+		function_a(tab[i++]);
 }
 
 template<typename T>
-void	print_one(const T &arg_1)
+void	print_one(T arg_1)
 {
 	std::cout << arg_1 << std::endl;
 }
--- module_07/ex01/source/main.cpp
+++ module_07/ex01/source/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 15:58:01 by tda-silv          #+#    #+#             */
-/*   Updated: 2023/02/13 10:15:35 by tda-silv         ###   ########.fr       */
+/*   Updated: 2023/02/13 11:25:17 by tda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,15 @@
 
 int main(void)
 {
-	int	tab[] = {1,2,3,4,5,6,7,8,9};
+	int			tab[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	float		tab2[] = {1.1f, 2.2f, 3.3f, 4.4f, 5.5f, 6.6f, 7.7f, 8.8f, 9.9f};
+	std::string	tab3[] = {"1.1", "2.2", "3.3", "4.4", "5.5", "6.6", "7.7", "8.8", "9.9"};
 
-	iter(tab, 9, &print_one);
+	::iter(tab, 9, &print_one);
+	std::cout << "\n";
+	::iter(tab2, 9, &print_one);
+	std::cout << "\n";
+	::iter(tab3, 9, &print_one);
 	return (0);
 }
 
