a8925c6855a1b9223344d580c7d8c982071b30fe
Thomas DA SILVA <thomas-78000@hotmail.fr>
2023-02-13 16:00:06 +0100

    m07 ex02 next

--- module_07/ex02/include/Array.hpp
+++ module_07/ex02/include/Array.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   template.hpp                                       :+:      :+:    :+:   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 14:49:55 by tda-silv          #+#    #+#             */
-/*   Updated: 2023/02/13 11:35:10 by tda-silv         ###   ########.fr       */
+/*   Updated: 2023/02/13 15:59:13 by tda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,101 @@ class Array
 	public:
 
 		Array(void);
-		Array(T);
+		Array(T n);
 		Array(const Array &src);
 		~Array(void);
 	
 		Array	&operator = (const Array &right);
-		
+		T		&operator [] (const T &x);
+
 	protected:
 
 	private:
-
+		T	*tab;
+		T	tab_size;
 };
+
+/* ************************************************************************** */
+/*                                                                            */
+/*   CONSTRUCTEUR															  */
+/*                                                                            */
+/* ************************************************************************** */
+template<typename T>
+Array<T>::Array(void)
+{
+	std::cout << "\033[00;02;03m" << "Array : Void constructor called" << "\033[00m" << std::endl;
+	tab = new T[0];
+	tab_size = 0;
+}
+
+template<typename T>
+Array<T>::Array(T n)
+{
+	std::cout << "\033[00;02;03m" << "Array : Number constructor called" << "\033[00m" << std::endl;
+	tab = new T[n];
+	tab_size = n;
+}
+
+/*   COPY CONSTRUCTEUR   **************************************************** */
+
+template<typename T>
+Array<T>::Array(const Array &src)
+{
+	std::cout << "\033[32;02;03m" << "Array : Copy constructor called" << "\033[00m" << std::endl;
+	
+}
+
+/* ************************************************************************** */
+/*                                                                            */
+/*   DESTRUCTEUR															  */
+/*                                                                            */
+/* ************************************************************************** */
+template<typename T>
+Array<T>::~Array(void)
+{
+	std::cout << "\033[31;01m" << "Array : Destructor called" << "\033[00m" << std::endl;
+	delete [] tab;
+}
+
+/* ************************************************************************** */
+/*                                                                            */
+/*   OPÉRATEUR																  */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*   OPÉRATEUR INTERNE   **************************************************** */
+
+template<typename T>
+Array<T>	&Array<T>::operator = (const Array &right)
+{
+	std::cout << "\033[33;02;03m" << "Array : Copy assignment operator called" << "\033[00m" << std::endl;
+	(void) right;
+	
+	return (*this);
+}
+
+template<typename T>
+T		&Array<T>::operator [] (const T &x)
+{
+	if (x >= 0 && x <= tab_size - 1)
+		return (tab[x]);
+	else
+		throw (std::exception());
+}
+
+/*   OPÉRATEUR EXTERNE   **************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*   MÉTHODE																  */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*   MÉTHODE PUBLIC   ******************************************************* */
+
+/*   MÉTHODE PRIVATE   ****************************************************** */
+
+/* ************************************************************************** */
+
 
 #endif
--- module_07/ex02/source/main.cpp
+++ module_07/ex02/source/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 15:58:01 by tda-silv          #+#    #+#             */
-/*   Updated: 2023/02/13 11:25:17 by tda-silv         ###   ########.fr       */
+/*   Updated: 2023/02/13 15:58:18 by tda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,22 @@
 
 int main(void)
 {
-	int			tab[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-	float		tab2[] = {1.1f, 2.2f, 3.3f, 4.4f, 5.5f, 6.6f, 7.7f, 8.8f, 9.9f};
-	std::string	tab3[] = {"1.1", "2.2", "3.3", "4.4", "5.5", "6.6", "7.7", "8.8", "9.9"};
+	Array<int>	new_empty_tab;
+	Array<int>	new_tab(3);
 
-	::iter(tab, 9, &print_one);
-	std::cout << "\n";
-	::iter(tab2, 9, &print_one);
-	std::cout << "\n";
-	::iter(tab3, 9, &print_one);
+	new_tab[0] = 0;
+	new_tab[1] = 1;
+	new_tab[2] = 2;
+
+	std::cout << new_tab[0] << std::endl;
+	std::cout << new_tab[1] << std::endl;
+	std::cout << new_tab[2] << std::endl;
+
+	Array<int>	copy_tab(new_tab);
+
+	std::cout << copy_tab[0] << std::endl;
+	std::cout << copy_tab[1] << std::endl;
+	std::cout << copy_tab[2] << std::endl;
 	return (0);
 }
 
